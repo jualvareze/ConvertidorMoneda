92a860ecbee8720c35e973f02865fdb407679d67
jualvareze <ju.alvareze@gmail.com>
2023-01-08 18:38:13 -0400

    agregar trycatch

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,23 +2,29 @@ const filterCurrencies = ['dolar', 'euro', 'uf']
 
 
 async function getCurrencies() {
-    const res = await fetch("https://mindicador.cl/api/")
-    const data = await res.json()
-    const CurrencyList = filterCurrencies.map((currency) => {
-        return {
-            code: data[currency].codigo,
-            value: data[currency].valor
-        }
-    })
+    try {
+        const res = await fetch("https://mindicador.cl/api/")
+        const data = await res.json()
+        const CurrencyList = filterCurrencies.map((currency) => {
+            return {
+                code: data[currency].codigo,
+                value: data[currency].valor
+            }
+        })
 
 
-    CurrencyList.forEach((localCurrency) => {
-        const List = document.querySelector('#list_Currency')
-        const option = document.createElement('option')
-        option.value = localCurrency.value
-        option.text = localCurrency.code
-        List.append(option)
-    })
+        CurrencyList.forEach((localCurrency) => {
+            const List = document.querySelector('#list_Currency')
+            const option = document.createElement('option')
+            option.value = localCurrency.value
+            option.text = localCurrency.code
+            List.append(option)
+        })
+    } catch (error) {
+        alert("Ocurrio un error al obtener los datos")
+        console.log("detalles de error " + error)
+    }
+
 }
 
 const Calcular = () => {
@@ -38,31 +44,37 @@ const Calcular = () => {
 }
 
 const DibujarGrafico = async (currency) => {
-    const ctx = document.querySelector('#grafico')
-    ctx.innerHTML = ""
-    const res = await fetch("https://mindicador.cl/api/" + currency)
-    const datos = await res.json()
-    const dias = datos.serie.slice(0, 10)
-    console.log(dias)
-    //comienzo del grafico
-    const data = {
-        labels: dias.map((item) => item.fecha.substring(0,10)),
-        datasets: [{
-            label: currency,
-            data: dias.map((item) => item.valor),
-            fill: false,
-            borderColor: 'rgb(0, 51, 0)',
-            tension: 0.1
-        }]}
-
+    try {
+        const ctx = document.querySelector('#grafico')
+        const res = await fetch("https://mindicador.cl/api/" + currency)
+        const datos = await res.json()
+        const dias = datos.serie.slice(0, 10)
+        console.log(dias)
+        //comienzo del grafico
+        const data = {
+            labels: dias.map((item) => item.fecha.substring(0, 10)),
+            datasets: [{
+                label: currency,
+                data: dias.map((item) => item.valor),
+                fill: false,
+                borderColor: 'rgb(0, 51, 0)',
+                tension: 0.1
+            }]
+        }
+    
         const config = {
             type: 'line',
             data: data,
         };
-        const chart = new Chart(ctx,config)
-    };
+        const chart = new Chart(ctx, config)
+    } catch (error) {
+        alert("Ocurrio un error al dibujar el grafico")
+        console.log("detalles de error " + error)
+    }
 
-    //final del grafico
+};
+
+//final del grafico
 
 
 const btnCalcular = document.querySelector('#btnCalc')
